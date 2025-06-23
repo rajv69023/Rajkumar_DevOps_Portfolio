96f40afaaf02563eccccc5da01e314d9203cea68
rajv69023 <rajv69023@gmail.com>
2025-06-23 20:06:14 +0530

    add new certifacite

--- client/src/components/experience-section.tsx
+++ client/src/components/experience-section.tsx
@@ -38,7 +38,7 @@ export default function ExperienceSection() {
   ];
 
 
-  return (
+return (
     <section id="experience" className="py-20 bg-white">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <motion.div
